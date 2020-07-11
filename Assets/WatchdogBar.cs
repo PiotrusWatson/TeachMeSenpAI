5145b60dcc258c8cdf8473491d1c615a96f45489
Matthew McConnell <matthewmcconnell04@gmail.com>
2020-07-11 10:22:23 +0000

    Added CTRL Watchdog Bar
    Added a simple watchdog bar that fills up and decays in value. Gonna keep tweaking this and adding one or two things.

---
+++ Assets/WatchdogBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WatchdogBar : MonoBehaviour
+{
+    
+    public Slider slider;
+    public float decayRate; // the rate at which the anger intolerance goes down
+    public float increaseRate; // the rate at which the anger intolerance goes up
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Have the slider and decrease over time, increase when CTRL is pressed*/
+        if (Input.GetKey(KeyCode.LeftControl))
+            slider.value += increaseRate;
+        else
+            slider.value -= decayRate;
+    }
+}
