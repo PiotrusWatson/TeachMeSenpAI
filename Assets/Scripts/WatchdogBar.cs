8e069243a177914c9966da5df3381cb3c3586ec2
Matthew McConnell <matthewmcconnell04@gmail.com>
2020-07-12 17:09:34 +0100

    Tweaked some stuff

--- Assets/Scripts/WatchdogBar.cs
+++ Assets/Scripts/WatchdogBar.cs
@@ -18,7 +18,7 @@ public class WatchdogBar : MonoBehaviour
 
 
         /*Have the slider and decrease over time, increase when CTRL is pressed*/
-        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.Tab))
+        if (Input.GetKey(KeyCode.LeftControl))
             slider.value += increaseRate;
         else
             slider.value -= decayRate;
